daa67637d8081ef20485c198d8c8cdfd1d6a5898
Bojidar21 <b.dimitrov.2004@gmail.com>
2023-01-10 00:24:53 +0200

    deleted:    css/email.css

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,24 @@
-function myFunction() {
+function menu() {
   var x = document.querySelector(".hamburger_nav");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
+}
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos>currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+    document.querySelector(".mobile_header").style.top ="0"
+  } else {
+    document.querySelector("header").style.top = "-85px"
+    document.querySelector(".mobile_header").style.top ="-400px"
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
 }
