d18f95d98ba98b7979fda1f4a26482bcbd2c3fe7
Bojidar21 <b.dimitrov.2004@gmail.com>
2023-01-10 00:44:44 +0200

    modified:   css/header.css

--- js/nav.js
+++ js/nav.js
@@ -6,15 +6,19 @@ function menu() {
     x.style.display = "block";
   }
 }
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos>currentScrollPos) {
-    document.querySelector("header").style.top = "0";
-    document.querySelector(".mobile_header").style.top ="0"
+    document.querySelector(".desktop_header").style.top = "0";
+    document.querySelector(".mobile_header").style.top = "0";
+    document.querySelector(".hamburger_nav").style.top = "0"
+
   } else {
-    document.querySelector("header").style.top = "-85px"
-    document.querySelector(".mobile_header").style.top ="-400px"
+    document.querySelector(".desktop_header").style.top = "-85px"
+    document.querySelector(".mobile_header").style.top = "-85px"
+    document.querySelector(".hamburger_nav").style.top = "-400px"
   }
   prevScrollpos = currentScrollPos;
 }
