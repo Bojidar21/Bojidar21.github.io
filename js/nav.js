77f06138a3bafd6020890c7e2671789f14ded718
Bojidar21 <b.dimitrov.2004@gmail.com>
2023-01-12 01:28:23 +0200

    modified:   css/footer.css

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,15 @@ function menu() {
   var x = document.querySelector(".hamburger_nav");
   if (x.style.display === "block") {
     x.style.display = "none";
+    document.querySelector(".hamburger_nav").style.width = "0"
+    document.querySelector("body").style.marginLeft = "0"
+    document.body.style.backgroundColor = "white"
+
   } else {
     x.style.display = "block";
+    document.querySelector(".hamburger_nav").style.width = "250px";
+    document.querySelector("body").style.marginLeft = "250px"
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
   }
 }
 
@@ -13,16 +20,21 @@ window.onscroll = function() {
   if (prevScrollpos>currentScrollPos) {
     document.querySelector(".desktop_header").style.top = "0";
     document.querySelector(".mobile_header").style.top = "0";
-    document.querySelector(".hamburger_nav").style.top = "0"
 
   } else {
     document.querySelector(".desktop_header").style.top = "-85px"
     document.querySelector(".mobile_header").style.top = "-85px"
-    document.querySelector(".hamburger_nav").style.top = "-400px"
   }
   prevScrollpos = currentScrollPos;
 }
 
 function myFunction(x) {
   x.classList.toggle("change");
+}
+let toTopButton = document.getElementById("top_button")
+
+
+function GoToTop() {
+ document.body,screenTop = 0
+ document.documentElement.scrollTop = 0
 }
